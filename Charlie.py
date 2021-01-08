8d68b381f65c4d46421640f45eb9b54329a97ddb
MaxwellDAnderson <maxwell.d.anderson@gmail.com>
2021-01-07 20:38:53 -0600

    cleaned code up

--- Charlie.py
+++ Charlie.py
@@ -8,8 +8,6 @@ e = b + " " + c
 
 print(b, c, d)
 
-# firstInput = input(e)
-
 endDoc = "\\\\\\"
 
 while True:
@@ -20,7 +18,6 @@ while True:
     n = input(i)
     if n != endDoc:
         continue
-    # print(g, h)
     else:
         break
 
@@ -28,23 +25,3 @@ j = datetime.datetime.utcnow()
 k = str(j)
 m = "UTC: ---END OF DOCUMENT---"
 print(k, m)
-
-'''
-
-x = datetime.datetime.utcnow()
-y = "UTC:"
-z = str(x)
-
-a = z + " " + y
-print(x, y, "---START OF DOCUMENT---")
-userInput = input(a)
-
-
-if userInput.casefold() == "yes":
-    print(x, y)
-else:
-    print(x, y, "Okay, goodbye!")
-'''
-
-# input("Would you like to know the date and time?")
-# print(x)
